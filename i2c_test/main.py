83b14bfdbbe43359e9ca2a83f03b83bc8dae7f84
rafachurre <rafachurre@gmail.com>
2017-11-16 12:46:16 +0100

    [FIX] correct index when printing the message sent

--- i2c_test/main.py
+++ i2c_test/main.py
@@ -94,7 +94,7 @@ while True:
                 continue
 
         writeNumber(send_code)
-        print "\nMessage sent (from Master to Slave): ", bMessageStyle.OKGREEN, i2cRequestMessagesDictionary[key], bMessageStyle.ENDC,
+        print "\nMessage sent (from Master to Slave): ", bMessageStyle.OKGREEN, i2cRequestMessagesDictionary[send_code], bMessageStyle.ENDC,
         # sleep one second
         time.sleep(1)
 
